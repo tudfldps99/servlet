b5c21c60b97cfb4b38aa80ecccf4f3ee3a6c2537
tudfldps99 <tudfldps99@naver.com>
2024-06-18 09:24:33 +0900

    20240618 Section2 HTTP 요청 데이터_API메시지 바디_JSON

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter     // lombok 사용
+public class HelloData {
+
+        private String username;
+        private int age;
+
+}
--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,5 +1,7 @@
 package hello.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,18 +13,27 @@ import org.springframework.util.StreamUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
-public class RequestBodyStringServlet extends HttpServlet {
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     // ctrl + o 로 function "service" 검색 -> protected 선택
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //super.service(req, resp);
 
-        // 요청 raw(Text) = hello!
+        // 요청 raw(JSON) = {"username": "hello", "age": 20}
         ServletInputStream inputStream = request.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-        System.out.println("messageBody = " + messageBody);         // hello!
+
+        System.out.println("messageBody = " + messageBody);             // {"username": "hello", "age": 20}
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData.username = " + helloData.getUsername());  // hello
+        System.out.println("helloData.age = " + helloData.getAge());            // 20
+
         response.getWriter().write("ok");
     }
 }
